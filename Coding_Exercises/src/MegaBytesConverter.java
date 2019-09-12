84eb487393bc90273c5d87a09464e70dda3360f8
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-09-12 23:03:59 +0200

    exercise 3 - BarkingDog move method 'main' to top of file

--- Coding_Exercises/src/MegaBytesConverter.java
+++ Coding_Exercises/src/MegaBytesConverter.java
@@ -1,5 +1,11 @@
 public class MegaBytesConverter {
 
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500); // 2500 KB = 2 MB and 452 KB
+        printMegaBytesAndKiloBytes(-1024); // Invalid Value
+        printMegaBytesAndKiloBytes(5000); // 5000 KB = 4 MB and 904 KB
+    }
+
     public static void printMegaBytesAndKiloBytes(int kiloBytes) {
         if (kiloBytes >= 0) {
             int megaBytes = kiloBytes / 1024;
@@ -8,11 +14,5 @@ public class MegaBytesConverter {
         } else {
             System.out.println("Invalid Value");
         }
-    }
-
-    public static void main(String[] args) {
-        printMegaBytesAndKiloBytes(2500); // 2500 KB = 2 MB and 452 KB
-        printMegaBytesAndKiloBytes(-1024); // Invalid Value
-        printMegaBytesAndKiloBytes(5000); // 5000 KB = 4 MB and 904 KB
     }
 }
--- Coding_Exercises/src/SpeedConverter.java
+++ Coding_Exercises/src/SpeedConverter.java
@@ -1,5 +1,14 @@
 public class SpeedConverter {
 
+    public static void main(String[] args) {
+        printConversion(0); // 0
+        printConversion(1.5); // 1
+        printConversion(10.25); // 6
+        printConversion(-5.6); // Invalid Value
+        printConversion(25.42); // 16
+        printConversion(75.114); // 47
+    }
+
     public static long toMilesPerHour(double kilometersPerHour) {
         return (kilometersPerHour >= 0) ? Math.round(kilometersPerHour/1.609) : -1;
     }
@@ -11,14 +20,5 @@ public class SpeedConverter {
         } else {
             System.out.println("Invalid Value");
         }
-    }
-
-    public static void main(String[] args) {
-        printConversion(0); // 0
-        printConversion(1.5); // 1
-        printConversion(10.25); // 6
-        printConversion(-5.6); // Invalid Value
-        printConversion(25.42); // 16
-        printConversion(75.114); // 47
     }
 }
