61cd44e83f746315213342c30274800ebba71278
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-09-12 22:45:13 +0200

    exercise 1 - speed converter

---
+++ Coding_Exercises/src/SpeedConverter.java
@@ -0,0 +1,24 @@
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        return (kilometersPerHour >= 0) ? Math.round(kilometersPerHour/1.609) : -1;
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        long milesPerHour = toMilesPerHour(kilometersPerHour);
+        if (milesPerHour >= 0) {
+            System.out.println( kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        } else {
+            System.out.println("Invalid Value");
+        }
+    }
+
+    public static void main(String[] args) {
+        printConversion(0); // 0
+        printConversion(1.5); // 1
+        printConversion(10.25); // 6
+        printConversion(-5.6); // Invalid Value
+        printConversion(25.42); // 16
+        printConversion(75.114); // 47
+    }
+}
