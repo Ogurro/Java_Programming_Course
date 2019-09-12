50897e0f9ea27862217ecbc8f6a4a3cd1b351d94
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-09-12 23:23:56 +0200

    exercise 4 - Leap Year

---
+++ Coding_Exercises/src/LeapYear.java
@@ -0,0 +1,26 @@
+public class LeapYear {
+
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(1700)); // false
+        System.out.println(isLeapYear(1600)); // true
+        System.out.println(isLeapYear(2000)); // true
+        System.out.println(isLeapYear(2400)); // true
+        System.out.println(isLeapYear(-1600)); // false
+        System.out.println(isLeapYear(10000)); // false
+        System.out.println(isLeapYear(2012)); // true
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year >= 1 && year <= 9999) {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 != 0) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
