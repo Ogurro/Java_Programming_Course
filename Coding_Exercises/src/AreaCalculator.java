a3e5d65171c9f208b866c8f643cca94360c45f4c
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-09-13 16:55:12 +0200

    exercise 8 - area calculator

---
+++ Coding_Exercises/src/AreaCalculator.java
@@ -0,0 +1,24 @@
+public class AreaCalculator {
+
+    public static void main(String[] args) {
+        System.out.println(area(5.0d)); // 78.53975
+        System.out.println(area(-1d)); // -1
+        System.out.println(area(5.0d, 4.0d)); // 20.0
+        System.out.println(area(-1d, 4.0d)); // -1
+        System.out.println(area(5.0d, -1)); // -1
+    }
+
+    private static double area(double radius) {
+        if (radius < 0) {
+            return -1d;
+        }
+        return Math.pow(radius, 2) * Math.PI;
+    }
+
+    private static double area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1d;
+        }
+        return x * y;
+    }
+}
