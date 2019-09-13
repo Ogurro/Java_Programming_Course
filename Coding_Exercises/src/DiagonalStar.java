ca1e181516a60b1f120140fca6f51be184ea5a9f
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-09-14 00:00:54 +0200

    next package of exercises

---
+++ Coding_Exercises/src/DiagonalStar.java
@@ -0,0 +1,39 @@
+public class DiagonalStar {
+
+    private static final String INVALID_VALUE_MSG = "Invalid Value";
+
+    public static void main(String[] args) {
+        printSquareStar(5);
+        System.out.println();
+        System.out.println("-----------------");
+        System.out.println();
+        printSquareStar(8);
+        System.out.println();
+        System.out.println("-----------------");
+        System.out.println();
+        printSquareStar(3);
+    }
+
+    private static void printSquareStar(int number) {
+        if (number < 5) {
+            System.out.println(INVALID_VALUE_MSG);
+        } else {
+            for (int i = 1; i <= number; i++) {
+                StringBuilder newRowBuilder = new StringBuilder();
+                if ((i == 1) || (i == number)) {
+                    newRowBuilder.append("*".repeat(number));
+                    System.out.println(newRowBuilder.toString());
+                } else {
+                    for (int j = 1; j <= number; j++) {
+                        if ((j == 1) || (j == number) || (j == i) || (i == (number - j + 1))) {
+                            newRowBuilder.append("*");
+                        } else {
+                            newRowBuilder.append(' ');
+                        }
+                    }
+                    System.out.println(newRowBuilder.toString());
+                }
+            }
+        }
+    }
+}
