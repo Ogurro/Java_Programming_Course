e1224cf51df8d16fab7fb12a4d0bd8747c744897
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-09-13 18:16:33 +0200

    exercise 10 nd 11

--- Coding_Exercises/src/LeapYear.java
+++ Coding_Exercises/src/LeapYear.java
@@ -10,13 +10,11 @@ public class LeapYear {
         System.out.println(isLeapYear(2012)); // true
     }
 
-    private static boolean isLeapYear(int year) {
+    static boolean isLeapYear(int year) {
         if (year >= 1 && year <= 9999) {
             if (year % 4 == 0) {
                 if (year % 100 == 0) {
-                    if (year % 400 != 0) {
-                        return false;
-                    }
+                    return year % 400 == 0;
                 }
                 return true;
             }
