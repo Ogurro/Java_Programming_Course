079083fd0b3caa4e133b76102a78f92db967d24c
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-09-14 20:13:22 +0200

    next package of exercises

---
+++ Coding_Exercises/src/InputCalculator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class InputCalculator {
+
+    public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
+    }
+
+    private static void inputThenPrintSumAndAverage() {
+        int sumOfInputs = 0;
+        int countOfInputs = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            if (scanner.hasNextInt()) {
+                sumOfInputs += scanner.nextInt();
+                countOfInputs++;
+            } else {
+                long avg = Math.round(((double) sumOfInputs) / ((double) countOfInputs));
+                System.out.println(String.format("SUM = %d AVG = %d", sumOfInputs, avg));
+                break;
+            }
+            scanner.nextLine();
+        }
+        scanner.close();
+    }
+}
