48ba86b67355e24949b844eabb527b378f2aec71
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-09-15 17:33:19 +0200

    OOP exercises

---
+++ src/exercises/oop/Circle.java
@@ -0,0 +1,18 @@
+package exercises.oop;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = Math.max(radius, 0);
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public double getArea() {
+        return (Math.pow(this.radius,2) * Math.PI);
+    }
+}
---
+++ src/exercises/oop/Cylinder.java
@@ -0,0 +1,19 @@
+package exercises.oop;
+
+public class Cylinder extends Circle {
+
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = Math.max(height, 0);
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getVolume() {
+        return this.getArea() * this.height;
+    }
+}
