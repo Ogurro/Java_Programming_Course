9a32e624c1ff6ba2170b5dc5eff0874f9538085a
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-09-18 18:57:27 +0200

    advanced variables - arrays exercises

---
+++ src/exercises/advanced_variables/MinimumValue.java
@@ -0,0 +1,38 @@
+package exercises.advanced_variables;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinimumValue {
+
+    public static int findMinValueFromInput(int count) {
+        count = Math.max(count, 0);
+        return findMin(readIntegers(count));
+    }
+
+    private static int[] readIntegers(int count) {
+        int[] intArray = new int[count];
+        if (count > 0) {
+            Scanner scanner = new Scanner(System.in);
+            int i = 0;
+            while (i < intArray.length) {
+                System.out.print(String.format("Input number #%d: ", i + 1));
+                if (scanner.hasNextInt()) {
+                    intArray[i] = scanner.nextInt();
+                    i++;
+                } else {
+                    System.out.println("Invalid Input");
+                }
+            }
+        }
+        return intArray;
+    }
+
+    private static int findMin(int[] intArray) {
+        if (intArray.length > 0) {
+            Arrays.sort(intArray);
+            return intArray[0];
+        }
+        return -1;
+    }
+}
---
+++ src/exercises/advanced_variables/SortIntegerArray.java
@@ -0,0 +1,52 @@
+package exercises.advanced_variables;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortIntegerArray {
+
+    public static void printSortedArray(int arrayLength) {
+        int[] array = getIntegers(arrayLength);
+        printArray(sortArray(array));
+    }
+
+    private static int[] getIntegers(int arrayLength) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            System.out.print("input integer: ");
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    private static int[] sortArray(int[] array) {
+        Arrays.sort(array);
+        int temp;
+        for (int i = 0; i < array.length /2; i++){
+            temp = array[i];
+            array[i] = array[array.length -1 - i];
+            array[array.length -1 - i] = temp;
+        }
+//        boolean flag = true;
+//        int temp;
+//        while (flag) {
+//            flag = false;
+//            for (int i = 0; i < array.length - 1; i++) {
+//                if (array[i] < array[i +1]) {
+//                   temp = array[i];
+//                   array[i] = array[i+1];
+//                   array[i+1] = temp;
+//                   flag = true;
+//                }
+//            }
+//        }
+        return array;
+    }
+
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(String.format("Array @ %d =  %d", i, array[i]));
+        }
+    }
+}
