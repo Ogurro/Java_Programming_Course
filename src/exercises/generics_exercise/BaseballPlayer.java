eb51c60676ea36facc183200b9ca38efb5152293
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-09-30 18:01:38 +0200

    generics exercise

---
+++ src/exercises/generics_exercise/BaseballPlayer.java
@@ -0,0 +1,10 @@
+package exercises.generics_exercise;
+
+/**
+ * Created by dev on 17/10/2015.
+ */
+public class BaseballPlayer extends Player {
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
