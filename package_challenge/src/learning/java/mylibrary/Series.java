256ac8eebe1b01fa327dd55ee0f22f19419ab267
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-10-01 19:16:14 +0200

    package exercise - move comments for functions

--- package_challenge/src/learning/java/mylibrary/Series.java
+++ package_challenge/src/learning/java/mylibrary/Series.java
@@ -2,24 +2,25 @@ package learning.java.mylibrary;
 
 public class Series {
 
+    /**
+     * Returns the sum of {int} form 0 to n.
+     * If n < 0 @returns -1.
+     */
     public static long nSum(int n) {
-        /*
-         * Returns the sum of {int} form 0 to n.
-         * If n < 0 @returns -1.
-         */
         if (n < 0) {
             return -1;
         }
         return (n * (n + 1)) / 2;
     }
 
+
+    /**
+     * Returns the product of {int} form 1 to n.
+     * i.e. 1 * 2 * 3 * 4 ... * (n - 1) * n.
+     * If n = 0 @returns 1.
+     * * If n < 0 @returns -1.
+     */
     public static long factorial(int n) {
-        /*
-         * Returns the product of {int} form 1 to n.
-         * i.e. 1 * 2 * 3 * 4 ... * (n - 1) * n.
-         * If n = 0 @returns 1.
-         * * If n < 0 @returns -1.
-         */
         if (n < 0) {
             return -1;
         }
@@ -30,14 +31,14 @@ public class Series {
         return factorial;
     }
 
+    /**
+     * Returns the nth Fibonacci {int}. These are defined as:
+     * If n < 0 @returns -1.
+     * f(0) = 0
+     * f(1) = 1
+     * f(n) = f(n-1) + f(n-2)
+     */
     public static long fibonacci(int n) {
-        /*
-         * Returns the nth Fibonacci {int}. These are defined as:
-         * If n < 0 @returns -1.
-         * f(0) = 0
-         * f(1) = 1
-         * f(n) = f(n-1) + f(n-2)
-         */
         if (n < 0) {
             return -1;
         } else if (n == 0) {
