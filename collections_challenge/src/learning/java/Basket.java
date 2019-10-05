546b7cd6341b2c6813a7ede629cb8e3cfb3aca1c
Pawel Wilkolek <ogur.pawel@gmail.com>
2019-10-05 16:31:29 +0200

    collections challenge - extend Basket class and tests

--- collections_challenge/src/learning/java/Basket.java
+++ collections_challenge/src/learning/java/Basket.java
@@ -4,7 +4,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Basket {
+public class Basket implements Comparable<Basket> {
 
     private final String name;
     private final Map<StockItem, Integer> basketList;
@@ -19,8 +19,13 @@ public class Basket {
     }
 
     public int adjustBasket(StockItem item, int quantity) {
-        if ((item != null) && (quantity > 0)) {
+        if (item != null) {
             int inBasket = basketList.getOrDefault(item, 0);
+            if (quantity < 0) {
+                if ((inBasket + quantity) < 0) {
+                    return 0;
+                }
+            }
             basketList.put(item, inBasket + quantity);
             return inBasket;
         }
@@ -29,5 +34,56 @@ public class Basket {
 
     public Map<StockItem, Integer> items() {
         return Collections.unmodifiableMap(this.basketList);
+    }
+
+    public int getItemQuantity(String itemName) {
+        return getItemQuantity(new StockItem(itemName, 0.0));
+    }
+
+    public int getItemQuantity(StockItem item) {
+        if (basketList.containsKey(item)) {
+            return this.basketList.get(item);
+        }
+        return 0;
+    }
+
+    public void listItem() {
+        System.out.println("\nItems in basket: ");
+        for (Map.Entry<StockItem, Integer> item : this.basketList.entrySet()) {
+            StockItem temp = item.getKey();
+            System.out.println(String.format("\t%s, price: %.2f, quantity: %d",
+                    temp.getName(), temp.getPrice(), item.getValue()));
+        }
+    }
+
+
+    @Override
+    public int compareTo(Basket o) {
+        if (this == o) {
+            return 0;
+        }
+        if (o != null) {
+            return this.getName().compareTo(o.getName());
+        }
+        throw new NullPointerException("Null object not valid for compareTo");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if ((obj != null) && (this.getClass() == obj.getClass())) {
+            String objName = ((Basket) obj).getName();
+            return this.name.equals(objName);
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + 41;
     }
 }
--- collections_challenge/tests/learning/java/BasketTest.java
+++ collections_challenge/tests/learning/java/BasketTest.java
@@ -11,6 +11,8 @@ public class BasketTest {
     private static StockItem item1;
     private static StockItem item2;
     private static Basket basket;
+    private static Basket basket2;
+    private static Basket basket3;
 
     @Before
     public void setUp() {
@@ -20,19 +22,48 @@ public class BasketTest {
         stockList.addItem(item1);
         stockList.addItem(item2);
         basket = new Basket("My basket");
-
+        basket2 = new Basket("My basket 2");
+        basket3 = new Basket("My basket");
     }
 
     @Test
     public void adjustBasket() {
         int quantity = 5;
-
         //no items in basket should return 0
-        assertEquals(basket.adjustBasket(item1, quantity), 0);
+        assertEquals(0, basket.adjustBasket(item1, quantity));
         // item1 already in basket should return previous quantity
-        assertEquals(basket.adjustBasket(item1, quantity), quantity);
+        assertEquals(quantity, basket.adjustBasket(item1, quantity));
         // item2 not in basket should return 0;
-        assertEquals(basket.adjustBasket(item2, 15), 0);
+        assertEquals(0, basket.adjustBasket(item2, 15));
+    }
 
+
+    @Test
+    public void items() {
+        assertEquals(0, basket.items().size());
+        basket.adjustBasket(item1, 3);
+        assertEquals(1, basket.items().size());
+        basket.adjustBasket(item2, 3);
+        assertEquals(2, basket.items().size());
+        basket.adjustBasket(item1, 4);
+        assertEquals(2, basket.items().size());
+    }
+
+    @Test
+    public void compareTo() {
+        assertEquals(0, basket.compareTo(basket3));
+        assertEquals(0, basket3.compareTo(basket));
+
+        assertNotEquals(0, basket.compareTo(basket2));
+        assertNotEquals(0, basket2.compareTo(basket));
+    }
+
+    @Test
+    public void testEquals() {
+        assertEquals(basket, basket3);
+        assertEquals(basket3, basket);
+
+        assertNotEquals(basket, basket2);
+        assertNotEquals(basket2, basket);
     }
 }
